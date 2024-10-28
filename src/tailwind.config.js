193d1a73b631c0f25400ee8f3d58ef64234b89fc
Augusto Nicola <augusto.nicola@willdom.com>
2024-10-28 20:06:41 -0300

    Header y Estilos Básicos

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -3,7 +3,18 @@ module.exports = {
     './public/**/*.html'
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'exCeleste': "#008296",
+        'exVerde': "#67cbb0",
+        'exClaro': "#f0fafa",
+        'exOscuro': "#0a1f1f"
+      },
+      fontFamily: {
+        exTitulos: ['Raleway', 'serif'],
+        exTexto: ['Rubik', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 };
