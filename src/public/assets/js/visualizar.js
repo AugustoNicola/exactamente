caf5880367b4f0661c99207e1b6bc031d95d4f26
Augusto Nicola <augusto.nicola@willdom.com>
2024-11-30 22:27:26 -0300

    Primer render de grafo

--- src/public/assets/js/visualizar.js
+++ src/public/assets/js/visualizar.js
@@ -1,8 +1,196 @@
 import listado from './data/listado.js';
 import Alpine from 'https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/module.esm.js';
+import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
+
+function svgPos(dimension, porcentaje) {
+    if (porcentaje < 0.0 || porcentaje > 100.0) {
+        throw new Error("Porcentaje inválido!")
+    }
+    return Number((porcentaje / 100.0) * dimension)
+}
+
+function estilosNodo(v) {
+    switch (v.estilo) {
+        case "verde":
+            return {
+                "color-borde": "#67cbb0",
+                "color-interno": "#b0eedd"
+            }
+        case "celeste":
+            return {
+                "color-borde": "#008296",
+                "color-interno": "#78cfdc"
+            }
+        case "gris":
+        default:
+            return {
+                "color-borde": "#464646",
+                "color-interno": "#aeaeae"
+            }
+    }
+}
+
+function estilosArista(e) {
+    switch (e.estilo) {
+        case "verde":
+            return {
+                "stroke": "#387467",
+                "stroke-width": 15
+            }
+        case "celeste":
+            return {
+                "stroke": "#008296",
+                "stroke-width": 15
+            }
+        case "gris":
+        default:
+            return {
+                "stroke": "#2c2c2c",
+                "stroke-width": 15
+            }
+    }
+}
+
+function renderGrafo(nodos, aristas) {
+    const width = document.getElementById('canvas-container').clientWidth;
+    const height = document.getElementById('canvas-container').clientHeight;
+    
+    const svg = d3.select("#canvas-visualizador")
+        .attr("width", width)
+        .attr("height", height);
+    
+    // Aristas
+    svg.selectAll("line")
+    .data(aristas, e => `${e.fuente}-${e.destino}`) // creamos IDs unicos para las aristas
+    .join(
+        enter => enter.append("line")
+        .attr("x1", e => svgPos(width, nodos[e.fuente].x))
+        .attr("y1", e => svgPos(height, nodos[e.fuente].y))
+        .attr("x2", e => svgPos(width, nodos[e.destino].x))
+        .attr("y2", e => svgPos(height, nodos[e.destino].y))
+        .attr("stroke", e => estilosArista(e)["stroke"])
+        .attr("stroke-width", e => estilosArista(e)["stroke-width"]),
+        update => update.attr("stroke", e => estilosArista(e)["stroke"]),
+        exit => exit.remove()
+    );
+    
+    if(aristas.length > 0 && aristas[0].label != null) {
+        // Cajas de las aristas
+        svg.selectAll(".edge-square")
+            .data(aristas, e => `${e.source}-${e.target}`)
+            .join(
+                enter => enter.append("rect")
+                .attr("x", e => {
+                    const midX = (svgPos(width, nodos[e.fuente].x) + svgPos(width, nodos[e.destino].x))/2.0
+                    return midX - 40.0/2.0;
+                })
+                .attr("y", e => {
+                    const midY = (svgPos(height, nodos[e.fuente].y) + svgPos(height, nodos[e.destino].y))/2.0
+                    return midY - 40.0/2.0;
+                })
+                .attr("width", 40)
+                .attr("height", 40)
+                .attr("fill", e => estilosArista(e)["stroke"])
+            );
+            
+        // Pesos de las aristas
+        svg.append("g")
+            .selectAll("text")
+            .data(aristas, e => `${e.source}-${e.target}`)
+            .join(
+                enter => enter.append("text")
+                    .attr("x", e => {
+                        const midX = (svgPos(width, nodos[e.fuente].x) + svgPos(width, nodos[e.destino].x))/2.0
+                        return midX;
+                    })
+                    .attr("y", e => {
+                        const midY = (svgPos(height, nodos[e.fuente].y) + svgPos(height, nodos[e.destino].y))/2.0
+                        return midY + 8.0;
+                    })
+                    .text(e => e.label)
+                    .attr("text-anchor", "middle")
+                    .attr("font-size", "24px")
+                    .attr("font-weight", "bold")
+                    .attr("font-family", "Rubik, serif")
+                    .attr("fill", "#ffffff")
+            );
+    }
+    
+    // Bordes de los nodos
+    svg.append("g")
+        .selectAll("circle")
+        .data(Object.entries(nodos), ([id, nodo]) => id)
+        .join(
+            enter => enter.append("circle")
+                .attr("cx", ([, nodo]) => svgPos(width, nodo.x))
+                .attr("cy", ([, nodo]) => svgPos(height, nodo.y))
+                .attr("r", 55)
+                .attr("fill", ([, nodo]) => estilosNodo(nodo)["color-borde"])
+        );
+    
+    // Cuerpo de los nodos
+    svg.append("g")
+    .selectAll("circle")
+    .data(Object.entries(nodos), ([id, nodo]) => id)
+    .join(
+        enter => enter.append("circle")
+            .attr("cx", ([, nodo]) => svgPos(width, nodo.x))
+            .attr("cy", ([, nodo]) => svgPos(height, nodo.y))
+            .attr("r", 45)
+            .attr("fill", ([, nodo]) => estilosNodo(nodo)["color-interno"])
+    );
+    
+    // IDs de los nodos
+    svg.append("g")
+        .selectAll("text")
+        .data(Object.entries(nodos), ([id, nodo]) => id)
+        .join(
+            enter => enter.append("text")
+                .attr("x", ([, nodo]) => svgPos(width, nodo.x))
+                .attr("y", ([, nodo]) => svgPos(height, nodo.y) - 8)
+                .text(([id, ]) => id)
+                .attr("text-anchor", "middle")
+                .attr("font-size", "32px")
+                .attr("font-weight", "bold")
+                .attr("font-family", "Rubik, serif")
+                .attr("fill", "#000000")
+        );
+    
+    // Labels de los nodos
+    svg.append("g")
+        .selectAll("text")
+        .data(Object.entries(nodos), ([id, nodo]) => id)
+        .join(
+            enter => enter.append("text")
+                .attr("x", ([, nodo]) => svgPos(width, nodo.x))
+                .attr("y", ([, nodo]) => svgPos(height, nodo.y) + 30)
+                .text(([, nodo]) => nodo.label)
+                .attr("text-anchor", "middle")
+                .attr("font-size", "24px")
+                .attr("font-weight", "bold")
+                .attr("font-family", "Rubik, serif")
+                .attr("fill", "#000000")
+        );
+}
 
 document.addEventListener('alpine:init', () => {
 	Alpine.data('listado', listado)
+    
+    let nodos = {
+        1: {id: "1", label: "A", estilo: "gris", x: 60.0, y: 35.0},
+        2: {id: "2", label: "B", estilo: "gris", x: 30.0, y: 50.0},
+        3: {id: "3", label: "C", estilo: "gris", x: 70.0, y: 70.0},
+        4: {id: "4", label: "Mati", estilo: "gris", x: 80.0, y: 20.0}
+    }
+    
+    let aristas = [
+        {fuente: 1, destino: 2, label: "3", estilo: "gris", dirigido: false},
+        {fuente: 1, destino: 3, label: "5", estilo: "gris", dirigido: false},
+        {fuente: 3, destino: 2, label: "1", estilo: "gris", dirigido: false},
+        {fuente: 4, destino: 1, label: "9", estilo: "gris", dirigido: false}
+    ]
+    
+    renderGrafo(nodos, aristas)
 })
 
 window.Alpine = Alpine;
