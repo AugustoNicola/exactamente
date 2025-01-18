118e26e4d808b8bf141195d1be10528389f31557
Augusto Nicola <auguston64@gmail.com>
2025-01-18 13:53:28 -0300

    Arreglamos Race Condition con MathJax al cambiar tipado de italics

--- src/public/assets/js/markdown.js
+++ src/public/assets/js/markdown.js
@@ -3,10 +3,8 @@ import Alpine from 'https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/module.esm.
 document.addEventListener('alpine:init', () => {
 	document.querySelectorAll('.markdown').forEach(paragraph => {
 		paragraph.innerHTML = paragraph.innerHTML
-			//.replace(/(?:\*_|_\*)(.*?)(?:\*_|_\*)/g, (_, match) => `<span class="font-bold italic">${match}</span>`) // bold e italic
-			//.replace(/(?:\*`|`\*)(.*?)(?:\*`|`\*)/g, (_, match) => `<span class="font-exCodigo font-bold">${match}</span>`) // bold y codigo
 			.replace(/\*(.*?)\*(?!\*)/g, (_, match) => `<span class="font-bold">${match}</span>`) // bold
-			.replace(/_(.*?)_(?!_)/g, (_, match) => `<span class="italic">${match}</span>`) // italic
+			.replace(/__(.*?)__(?!__)/g, (_, match) => `<span class="italic">${match}</span>`) // italic
 			.replace(/`(.*?)`(?!`)/g, (_, match) => `<span class="font-exCodigo">${match}</span>`) // mono
 			;	
 	});
